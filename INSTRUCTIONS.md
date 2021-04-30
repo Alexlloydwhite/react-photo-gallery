19f99d49724ddacc34c4a1334a1d08c48498f3e3
Alex White <alexlloydwhite@gmail.com>
2021-04-29 20:34:58 -0500

    image toggles description on click

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -45,9 +45,9 @@ To practice passing values through `props` in React, you will build the gallery 
     [x] Iterate (loop over) the list of gallery data
     [x] Make GalleryItems
 [x] Create a new **component** called `GalleryItem.jsx` and pass it the individual gallery item via `props`. 
-    [] Update the `GalleryList` to use this component to display an image.
+    [x] Update the `GalleryList` to use this component to display an image.
     [] Swap the image with the description on click. Use [conditional rendering](https://reactjs.org/docs/conditional-rendering.html).
-    [] Display the number likes for each item and include a like button.
+    [x] Display the number likes for each item and include a like button.
     [] When the like button is clicked, use `Axios` to update (`PUT`) the like count `/gallery/like/:id`.
     [] Update the gallery each time a like button is clicked.
 
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,24 @@
 import './GalleryItem.css';
+import { useState } from 'react';
 
 function GalleryItem(props) {
+    // image is displayed default to TRUE
+    const [toggled, setToggled] = useState(true);
 
+    const toggleImage = () => {
+        setToggled(!toggled);
+        console.log('clicked!');
+    }
     return (
         <div class="galleryItem">
-            <img src={props.path} width="200" height="200" />
-            <p>Likes: {props.likes}</p>
+            {/* if image is toggled, display image */}
+            {toggled && <img src={props.path} 
+                onClick={toggleImage}
+                width="200" height="200" />}
+            {/* if image is clicked, display description */}
+            {!toggled && <p id="galleryItemDescription" onClick={toggleImage}>{props.description}</p>}
+            <p>{props.likes} people love this!</p>
+            <button type="button">Love It!</button>
         </div>
     )
 }
