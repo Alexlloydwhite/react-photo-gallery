8b838402b059b7b24db3afe66d113fd639bf30ac
Alex White <alexlloydwhite@gmail.com>
2021-05-02 14:49:38 -0500

    creating card, adding icons

--- src/components/GalleryCard/GalleryCard.jsx
+++ src/components/GalleryCard/GalleryCard.jsx
@@ -1,9 +1,24 @@
 // import './GalleryItem.css';
 import { useState } from 'react';
 import axios from 'axios';
-import Paper from '@material-ui/core/Paper'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import { makeStyles } from '@material-ui/core';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
-function GalleryItem({image, getGalleryObject}) {
+const useStyles = makeStyles({
+
+})
+
+function GalleryItem({ image, getGalleryObject }) {
+
+    const classes = useStyles();
+
     // image is displayed default to TRUE
     const [toggled, setToggled] = useState(true);
 
@@ -51,17 +66,40 @@ function GalleryItem({image, getGalleryObject}) {
 
     return (
         <div>
-            {/* if image is toggled, display image */}
-            {toggled && <img src={image.path}
-                onClick={toggleImage}
-                width="200" height="200" />}
-            {/* if image is clicked, display description */}
-            {!toggled && <p id="galleryItemDescription" onClick={toggleImage}>{image.description}</p>}
-            <p>{image.likes} people love this! </p>
-            <button data-id={image.id} onClick={addLike}>Love It!</button>
-            <button data-id={image.id} onClick={deleteImage}>Delete</button>
+            <Card>
+                <CardContent>
+                    <Typography color="Primary">
+                        {image.likes} people love this!
+                    </Typography>
+                    <CardMedia>
+                        <img 
+                            src={image.path} 
+                            height="200"
+                            width="200"
+                        />
+                    </CardMedia>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton>
+                        <DeleteForeverIcon />
+                    </IconButton>
+                </CardActions>
+            </Card>
+
         </div>
     )
 }
 
 export default GalleryItem;
+
+// {/* if image is toggled, display image */}
+// {toggled && <img src={image.path}
+// onClick={toggleImage}
+// width="200" height="200" />}
+// {/* if image is clicked, display description */}
+// {!toggled && <p id="galleryItemDescription" onClick={toggleImage}>{image.description}</p>}
+
+
+// <p>{image.likes} people love this! </p>
+// <button data-id={image.id} onClick={addLike}>Love It!</button>
+// <button data-id={image.id} onClick={deleteImage}>Delete</button>
