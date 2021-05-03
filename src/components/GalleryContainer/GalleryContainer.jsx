4efefa6c5a99f87d3f9f3bb9184f057aa62b4eb6
Alex White <alexlloydwhite@gmail.com>
2021-05-03 14:30:33 -0500

    removed for loop

--- src/components/GalleryContainer/GalleryContainer.jsx
+++ src/components/GalleryContainer/GalleryContainer.jsx
@@ -2,12 +2,9 @@ import GalleryCard from '../GalleryCard/GalleryCard'
 import Grid from '@material-ui/core/Grid'
 import { Container } from '@material-ui/core'
 
-// props are: galleryObject(stores data from DB), getGalleryObject (function to perform GET request for data)
 function GalleryList({galleryObject, getGalleryObject}) {
     return (
-        // container holds cards
         <Container>
-            {/* spacing for cards to display correctly on different screens */}
             <Grid container spacing={3}> 
                 {galleryObject.map(image=>
                     <Grid item key={image.id}
@@ -15,7 +12,6 @@ function GalleryList({galleryObject, getGalleryObject}) {
                         md={6}
                         lg={4}
                     >
-                        {/* passing props to card! */}
                         <GalleryCard image={image} getGalleryObject={getGalleryObject}/>
                     </Grid>
                 )}
